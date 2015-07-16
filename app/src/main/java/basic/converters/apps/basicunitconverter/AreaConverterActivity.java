73d7a4c15193d32dcdb64841c023735586e2c8e2
Edmar Gonçalves <ejsgon@gmail.com>
2015-07-16 16:27:34 -0700

    Committing files and changes for Area converter.

---
+++ app/src/main/java/basic/converters/apps/basicunitconverter/AreaConverterActivity.java
@@ -0,0 +1,18 @@
+package basic.converters.apps.basicunitconverter;
+
+import android.os.Bundle;
+
+import basic.converters.helper.AbstractConverter;
+import basic.converters.util.AreaUnit;
+
+
+public class AreaConverterActivity extends AbstractConverter {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState, this,
+                "distance", R.array.area_units,
+                R.layout.activity_area_converter,
+                this.getClass(), AreaUnit.class);
+    }
+}
---
+++ app/src/main/java/basic/converters/util/AreaUnit.java
@@ -0,0 +1,7 @@
+package basic.converters.util;
+
+/**
+ * Created by Edmar on 7/16/2015.
+ */
+public enum AreaUnit {
+}
