483258d468e8e38a1036f3bd2d54691f79a3e603
Edmar Gonçalves <ejsgon@gmail.com>
2015-08-12 22:51:54 -0700

    Adjusted table name.

--- app/src/main/java/basic/converters/apps/basicunitconverter/TemperatureConverterActivity.java
+++ app/src/main/java/basic/converters/apps/basicunitconverter/TemperatureConverterActivity.java
@@ -1,34 +1,16 @@
 package basic.converters.apps.basicunitconverter;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import basic.converters.helper.AbstractConverter;
-import basic.converters.util.ConversionEntriesDataSource;
-import basic.converters.util.ConversionEntry;
-import basic.converters.util.DistanceUnit;
 import basic.converters.util.TemperatureUnit;
-
-import java.util.List;
 
 public class TemperatureConverterActivity extends AbstractConverter {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState, this,
-            "distance", R.array.temperature_units,
+            "temperature", R.array.temperature_units,
                 R.layout.activity_temperature_converter,
                 this.getClass(), TemperatureUnit.class);
     }
