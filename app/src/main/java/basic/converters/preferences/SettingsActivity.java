777750bbdb9c65fc95cbb5777b9646d7b3a9f43a
Edmar Gonçalves <ejsgon@gmail.com>
2015-09-25 17:52:58 -0700

    Issue #38 Committing theme changes to settings activity.

--- app/src/main/java/basic/converters/preferences/SettingsActivity.java
+++ app/src/main/java/basic/converters/preferences/SettingsActivity.java
@@ -1,13 +1,12 @@
 package basic.converters.preferences;
 
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Edmar on 6/7/2015.
  */
-public class SettingsActivity extends PreferenceActivity{
+public class SettingsActivity extends AppCompatActivity{
 
     private static final String TAG = SettingsActivity.class.getSimpleName(); // tag to be used when logging
 
