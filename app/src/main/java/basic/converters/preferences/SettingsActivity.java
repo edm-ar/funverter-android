ebe896d2cc6069d3bd7d31f1fecda3f11ae1533c
Edmar Gonçalves <ejsgon@gmail.com>
2015-10-02 00:46:48 -0400

    Committing changes to feedback preferences.

--- app/src/main/java/basic/converters/preferences/SettingsActivity.java
+++ app/src/main/java/basic/converters/preferences/SettingsActivity.java
@@ -1,22 +1,36 @@
 package basic.converters.preferences;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import basic.converters.apps.basicunitconverter.R;
 
 /**
  * Created by Edmar on 6/7/2015.
  */
-public class SettingsActivity extends AppCompatActivity{
+public class SettingsActivity extends AppCompatActivity {
 
     private static final String TAG = SettingsActivity.class.getSimpleName(); // tag to be used when logging
+
+    private Preference sendFeedbackPref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Display the fragment as the main content
+        SettingsFragment fragment = new SettingsFragment();
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(android.R.id.content, fragment)
                 .commit();
+
     }
 }
--- app/src/main/java/basic/converters/preferences/SettingsFragment.java
+++ app/src/main/java/basic/converters/preferences/SettingsFragment.java
@@ -1,20 +1,67 @@
 package basic.converters.preferences;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.widget.Toast;
 
 import basic.converters.apps.basicunitconverter.R;
 
 /**
  * Created by Edmar on 6/7/2015.
  */
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener {
+
+    private static final int SEND_FEEDBACK_REQUEST = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Load the preferences from XML resource
         addPreferencesFromResource(R.xml.preferences);
+
+        Preference sendFeedbackPref = findPreference("pref_key_send_feedback");
+        sendFeedbackPref.setOnPreferenceClickListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"ejsgon@gmail.com"});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+
+        try {
+            startActivityForResult(Intent.createChooser(emailIntent,
+                    "Send feedback using: "), SEND_FEEDBACK_REQUEST);
+        } catch(ActivityNotFoundException e) {
+            Toast toast = Toast.makeText(getActivity(),
+                    "Oops...there doesn't seem to be any email clients available.",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == SEND_FEEDBACK_REQUEST) {
+            if(resultCode == 0) {
+                Toast toast = Toast.makeText(getActivity(),
+                        "Feedback sent :)",
+                        Toast.LENGTH_SHORT);
+                toast.show();
+            } else if(resultCode == -1) {
+                Toast toast = Toast.makeText(getActivity(),
+                        "Oops...something went wrong :(",
+                        Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
